c3604c468cdb866fc47e5775d212ae27e0a996a8
github <jiangbenyongs@163.com>
2019-04-23 00:01:22 +0800

    regist eurake

--- src/main/java/edu/xj/cloud/ProviderUserApplication.java
+++ src/main/java/edu/xj/cloud/ProviderUserApplication.java
@@ -2,7 +2,9 @@ package edu.xj.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProviderUserApplication {
 	public static void main(String[] args) {
